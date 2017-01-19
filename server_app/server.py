f15fb4a0bef34e417342ef0d382a0b65637b1319
Nicolas Chabanovsky <nicolas@chabanovsky.com>
2017-01-19 17:35:13 +0100

    Added the template folder.

--- server_app/server.py
+++ server_app/server.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/index.html")
+def index():
+    return render_template('layout.html')
 
 if __name__ == "__main__":
     app.run()
