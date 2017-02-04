50ce2429f5a583585e2651b2b49a0798a13d5b3a
Igor <urffin2006@gmail.com>
2017-02-04 23:09:25 +0300

    remove jquery, use promises

--- extension/association_tools.js
+++ extension/association_tools.js
@@ -8,111 +8,79 @@ var addAssociationHelpText = "Ассоциировать";
 var questionApiEndpoint = "https://api.stackexchange.com/2.2/questions/{id}?order=desc&sort=votes&site=stackoverflow"
 var commetsToQuestionApiEndpoint = "https://api.stackexchange.com/2.2/questions/{id}/comments?order=desc&sort=creation&site=ru.stackoverflow&filter=!9YdnSNaN(";
 
-init();
+checkPage(document.baseURI)
+    .then(loadCommentsFromLocalizedStackOverflow)
+    .then(checkCommentsLoaded)
+    .then(findTargetComment)
+    .then(findLinkToSO)
+    .then(getEnQuestionId)
+    .then(({ comment, linkToSOen, soEnQuestionId, absTime }) => loadQuestionFromStackOverflowInEnglish(soEnQuestionId).then(data => ({ comment, linkToSOen, enQuestion: data.items[0], absTime })))
+    .then(createAssociationBox)
+    .catch(handleWrongPage)
+    .then(processAddingAssociationLink);
 
-function init() {
-    var uri = document.baseURI;
-    if (!uri.includes(targetPage) || uri.includes(targetPageException))
-        return;
-
-    var localisedQuestionId = questionId(uri);
-    processExistedAssociation(localisedQuestionId);
-}
-
-function processAddingAssociationLink() {
-    $("#question .post-menu").append("<a id='associate-link' class=''>" + associateHelpText + "</a>");
-    $("#associate-link").click(function() {
-        var event;
-
-        if (document.createEvent) {
-            event = document.createEvent("HTMLEvents");
-            event.initEvent("click", true, true);
+function checkPage(uri) {
+    return new Promise((resolve, reject) => {
+        if (!uri.includes(targetPage) || uri.includes(targetPageException)) {
+            reject('wrong page')
         } else {
-            event = document.createEventObject();
-            event.eventType = "click";
+            resolve(questionId(uri));
         }
-        event.eventName = "click";
-        var element = $(".question .js-add-link")[0];
-        if (document.createEvent) {
-            element.dispatchEvent(event);
-        } else {
-            element.fireEvent("on" + event.eventType, event);
-        }
-
-        $(".question .comment-form textarea").text(associationTag + " ");
-        $(".question .comment-form input").val(addAssociationHelpText);
-        event.preventDefault();
     });
 }
+function handleWrongPage(rejectReason) {
+    if (rejectReason == 'wrong page') return Promise.reject();
+    console.error(rejectReason);
+}
+function checkCommentsLoaded(data) {
+    return (data == undefined || data.items.length == 0)
+        ? Promise.reject()
+        : data.items;
+}
+function findTargetComment(comments) {
+    return comments.find(comment => comment.body.toLowerCase().includes(associationTag)) || Promise.reject();
+}
+function findLinkToSO(comment) {
+    var tmpCommnetHtml = parseTemplate(`<div>${comment.body}</div>`);
 
-function processExistedAssociation(localisedQuestionId) {
-    function ifThereIsNoAssociation() {
-        processAddingAssociationLink();
+    var linkToSOen = tmpCommnetHtml.querySelector("a").href;
+    if (linkToSOen.length == 0) {
+        console.error("Unexpected comment structure");
+        return Promise.reject();
     }
-    loadCommentsFromLocalizedStackOverflow(localisedQuestionId, function(data) {
-        if (data == undefined || data.items.length == 0) {
-            ifThereIsNoAssociation();
-            return;
-        }
+    return { comment, linkToSOen }
+}
+function processAddingAssociationLink() {
+    document.querySelector("#question .post-menu").appendChild(parseTemplate(`<a id='associate-link' class=''>${associateHelpText}</a>`));
+    document.querySelector("#associate-link").addEventListener('click', function (e) {
+        document.querySelector(".question .js-add-link").click();
 
-        var uploadedTargetComment = null;
-        for (index = 0; index < data.items.length; index++) {
-            var item = data.items[index];
-            if (item.body.toLowerCase().includes(associationTag)) {
-                uploadedTargetComment = item;
-                break;
-            }
-        }
-
-        if (uploadedTargetComment == null) {
-            ifThereIsNoAssociation();
-            return;
-        }
-
-        var tmpCommnetHtml = "<div>" + uploadedTargetComment.body + "</div>";
-        var linkToSOen = $(tmpCommnetHtml).find("a").attr("href");
-        if (linkToSOen.length == 0) {
-            console.error("Unexpected comment structure");
-            ifThereIsNoAssociation();
-            return;
-        }
-
-        createAssociationBox(uploadedTargetComment, linkToSOen);
-
-    }, function() {
-        ifThereIsNoAssociation();
+        document.querySelector(".question .comment-form textarea").textContent = associationTag + " ";
+        document.querySelector(".question .comment-form input").value = addAssociationHelpText;
+        e.preventDefault();
     });
 }
-
-function createAssociationBox(uploadedTargetComment, linkToSOen) {
-    var absTime = (new Date(uploadedTargetComment.creation_date * 1000)).toISOString().replace("T", " ").replace(".000", "");
+function getEnQuestionId({comment, linkToSOen}) {
+    var absTime = (new Date(comment.creation_date * 1000)).toISOString().replace("T", " ").replace(".000", "");
     var soEnQuestionId = questionId(linkToSOen);
-
-    loadQuestionFromStackOverflowInEnglish(soEnQuestionId,
-        function(data) {
-            var tmp = associationBoxTemplate();
-            var template = $(tmp);
-            $(template).find(".association-author").text(uploadedTargetComment.owner.display_name);
-            $(template).find(".association-author").attr("href", uploadedTargetComment.owner.link);
-            $(template).find(".association-link").text(stripHtml(data.items[0].title));
-            $(template).find(".association-link").attr("href", linkToSOen);
-            //$(template).find(".relativetime-clean").text();
-            $(template).find(".relativetime-clean").attr("title", absTime);
-
-            var prevDisplayState = $("#comment-" + uploadedTargetComment.comment_id).css("display");
-            $("#comment-" + uploadedTargetComment.comment_id).css("display", "none");
-            $(".question > table > tbody > tr:first-child").after(template);
-            $(".association-as-comment").click(function(event) {
-                $("#comment-" + uploadedTargetComment.comment_id).css("display", prevDisplayState);
-                $(".association-root").css("display", "none");
-                event.preventDefault();
-            });
-        },
-        function() {
-            console.error("Could not load question #" + soEnQuestionId + " from Stack Overflow in English");
-        });
+    return { comment, linkToSOen, soEnQuestionId, absTime };
 }
+function createAssociationBox({comment, linkToSOen, enQuestion, absTime}) {
+    var template = associationBoxTemplate(comment.owner, linkToSOen, stripHtml(enQuestion.title), absTime);
+    var assocComment = document.querySelector(`#comment-${comment.comment_id}`)
+    if (assocComment)
+        assocComment.style.display = "none";
 
+    document.querySelector(".question > table > tbody > tr:first-child").insertAdjacentHTML('afterend',template);
+    document.querySelector(".association-as-comment").addEventListener('click', function (e) {
+        var assocComment = document.querySelector(`#comment-${comment.comment_id}`)
+        if (assocComment)
+            assocComment.style.display = "";
+
+        document.querySelector(".association-root").style.display = "none";
+        e.preventDefault();
+    });
+}
 function questionId(uri) {
     var id = /\/\d+\//.exec(uri)
     if (!id)
@@ -121,39 +89,45 @@ function questionId(uri) {
     return id[0].replace(/\//g, "");
 }
 
-function userId(uri) {
-    var id = /\d+/.exec(uri)
-    if (!id)
-        return -1
-
-    return id[0].replace(/\//g, "");
+function getJson(url) {
+    return fetch(url, { method: 'GET' })
+        .then((response) => response.json())
+        .catch((...args) => console.log('error request', ...args));
 }
-
-function loadHelper(url, onSuccess, onError) {
-    $.ajax({
-        url: url,
-        method: 'GET',
-        success: onSuccess,
-        error: onError
-    });
-}
-
-function loadQuestionFromStackOverflowInEnglish(id, onSuccess, onError) {
+function loadQuestionFromStackOverflowInEnglish(id) {
+    console.log('load en question');
     var url = questionApiEndpoint.replace(/\{id\}/g, id);
-    loadHelper(url, onSuccess, onError)
+    return getJson(url);
 }
-
-function loadCommentsFromLocalizedStackOverflow(id, onSuccess, onError) {
+function loadCommentsFromLocalizedStackOverflow(id) {
+    console.log('load comments');
     var url = commetsToQuestionApiEndpoint.replace(/\{id\}/g, id);
-    loadHelper(url, onSuccess, onError)
+    return getJson(url);
 }
-
 function stripHtml(html) {
     var div = document.createElement("div");
     div.innerHTML = html;
     return div.textContent || div.innerText || "";
 }
-
-function associationBoxTemplate() {
-    return '<tr class="association-root"><td class="special-status" colspan="2"><div class="question-status"><h2>' + associationHelpText + ' <a class="association-author"></a> <span class="relativetime-clean"></span></h2><p><b>Stack Overflow на английском</b>: <a class="association-link"></a><a class="association-as-comment comment-delete delete-tag" style="visibility: visible;" href="#"></a></p></div></td></tr>'
+function associationBoxTemplate(owner, linkToSOen, enQuestionTitle, absTime) {
+    return `
+    <tr class="association-root">
+        <td class="special-status" colspan="2">
+            <div class="question-status">
+                <h2>
+                    ${associationHelpText}
+                    <a class="association-author" href="${owner.link}">${owner.display_name}</a> <span title="${absTime}" class="relativetime-clean"></span>
+                </h2>
+                <p>
+                    <b>Stack Overflow на английском</b>: 
+                    <a class="association-link" href="${linkToSOen}">${enQuestionTitle}</a>
+                    <a class="association-as-comment comment-delete delete-tag" style="visibility: visible;" href="#"></a>
+                </p>
+            </div>
+        </td>
+    </tr>`
+}
+function parseTemplate(template) {
+    var parser = new DOMParser();
+    return parser.parseFromString(template, 'text/html').body.firstElementChild;
 }
