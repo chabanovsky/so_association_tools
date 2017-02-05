d6ffabeba55a8e03151b3722a3cb1171ed266de4
reinraus <reinraus@reinraus-Lenovo-G510>
2017-02-05 20:39:27 +0300

    Refactoring: function questionId

--- extension/association_tools.js
+++ extension/association_tools.js
@@ -82,11 +82,8 @@ function createAssociationBox({comment, linkToSOen, enQuestion, absTime}) {
     });
 }
 function questionId(uri) {
-    var id = /\/\d+\//.exec(uri)
-    if (!id)
-        return -1
-
-    return id[0].replace(/\//g, "");
+    var id = /\/(\d+)\//.exec(uri)
+    return id == null ? -1 : id[1];
 }
 
 function getJson(url) {
